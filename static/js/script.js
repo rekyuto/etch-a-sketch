ec40f7f5d0afb26efd67c8ee63fc3de7a0bfd846
rekyuto <62195454+rekyuto@users.noreply.github.com>
2024-11-05 09:50:26 -0800

    Added a reset button
    Added a reset button that completely erases the grid.

--- static/js/script.js
+++ static/js/script.js
@@ -4,12 +4,31 @@ const grid = document.querySelector("#grid");
 
 for (let i = 0; i < 256; i++) {
   const gridSquare = document.createElement('div');
+
   gridSquare.style.width = "40px";
   gridSquare.style.height = "40px";
   grid.appendChild(gridSquare);
 
+  // Makes a square black on hover
+
   gridSquare.addEventListener("mouseover", () => {
     gridSquare.style.backgroundColor = "black";
   });
+
+  // Resets the entire grid
+
+  const resetGrid = document.querySelector("#resetGrid");
+
+  resetGrid.addEventListener("click", () =>{
+    gridSquare.style.backgroundColor = "white";
+  });
 }
 
+/*
+const changeGridSize = document.querySelector("#changeGridSize");
+
+changeGridSize.addEventListener("click", () => {
+    const size = prompt("Enter a value from 1-100:");
+    
+});
+*/
